c090ec09e7de4aae46a912505a311d190389112c
Aviral Ale <69aviral@gmail.com>
2026-02-03 15:03:59 +0545

    Include Google Ads script in layout
    Added Google Ads script to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -30,6 +31,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2967783814418016"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         <div className="min-h-screen">{children}</div>
       </body>
